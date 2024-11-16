6ceccd5b38da3aac994a0705854f78769986ac09
JayIbrak <26jibrakovic@derryfield.org>
2024-11-15 21:47:12 -0500

    Lesson 12

--- Lesson12/PartB.java
+++ Lesson12/PartB.java
@@ -1,12 +1,38 @@
-package Lesson12 ;
-import java.util.Scanner;
+/*
+ * Jahja Ibrakovic
+ * Lesson 12
+ */
 
-public Min()
-{
+package Lesson12 ;
+public class PartB{
+
+public static double Smallest(double x, double y, double z)
+{   
+    double smallest = x;
+
+    if (y<x && y<z)
+    {
+        smallest = y;
+    }
+    else if (z<x && z<y)
+    {
+        smallest = z;
+    }
+    else{
+        return smallest;
+    }
+    return smallest;
 
 }
-public class PartB {
-    public static void main(String[] args) {
-        
+
+
+ public static void main(String[] args)
+    {
+        double num1 = 2.74;
+        double num2 = 4.37;
+        double num3 = 8.83;
+
+        System.out.println("The smallest number among " + num1 + ", " + num2 + ", " + num3 + " is " + Smallest(num1, num2, num3));
     }
+
 }
