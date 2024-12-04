6992a171119a6f0635358b3507db72411a013bac
JayIbrak <26jibrakovic@derryfield.org>
2024-12-04 13:22:58 -0500

    Part C

---
+++ Lesson14/PartC.java
@@ -0,0 +1,18 @@
+package Lesson14;
+public class PartC {
+    public static void main(String[] args) {
+        double[] numbers = {2.2, 1.0, 3.7, 4.1, 88.0};
+
+        double sum = 0.0;
+        double average;
+
+        for(double num : numbers)
+        {
+            System.out.println(num);
+            sum += num;
+        }
+    
+        average = sum/numbers.length;
+        System.out.println("Avg = " + average);
+ }
+}
